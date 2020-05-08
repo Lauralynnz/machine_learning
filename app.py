cb351a4ff77c95cc22a271b9fd8fc0e24c4b0859
lauralynnz <llstubler@gmail.com>
2020-05-07 21:13:10 -0700

    upload

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ model = pickle.load(open('model.pkl','rb'))
 # Flask Routes
 #################################################
 keys = {
-    "1":"Interest rate less than 10%",
-    "2":"Interest rate between 11% and 14%",
-    "3":"Interest rate between 14% and 18%",
-    "4":"Interest rate over 18%"
+    "1":"Sweet! You’re approved for this loan with an interest rate that is less than 10%!",
+    "2":"Congrats! Your loan is approved and your interest rate is in the range between 10% and 14%",
+    "3":"Congratulations on being approved for this loan. Your interest rate repayment will be between 14% and 18%",
+    "4":"Damn son! You’ve got your loan, but careful, you’re expected to pay back at an interest rate of 18% or more"
 }
 
 @app.route("/")
@@ -36,13 +36,27 @@ def form():
     loan_amnt=request.form["loan_amnt"]
     #data = [annual_inc, fico_range_low, term, loan_amnt] #request.get_json() #(force=True)
     data=pd.DataFrame({"annual_inc": [annual_inc], "fico_range_low": [fico_range_low], "term": [term], "loan_amnt": [loan_amnt]})
+    print(data)
+    X=pd.read_csv('features.csv')
+    data_df=pd.concat([X,data],ignore_index=True)
     scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(data)#.reshape(-1,1)
-
-    prediction = model.predict(data_scaled) 
-
+    
+    data_scaled = scaler.fit_transform(data_df)
+    print([data_scaled[-1]])
+    prediction = model.predict([data_scaled[-1]]) 
+    gif=""
     output = {"prediction":keys[str(prediction[0])]}
-    return render_template("index.html",output=output)
+    if(prediction[0] == 1):
+        gif="https://media.giphy.com/media/SWj2IjMLEvnOdaT9Sx/giphy.gif"
+    elif(prediction[0] == 2):
+        gif="https://media.giphy.com/media/ibXYwKoCdGuATx1alX/giphy.gif"
+    elif(prediction[0] == 3):
+        gif="https://media.giphy.com/media/JoP7eSJ4pgClIO0opn/giphy.gif"
+    else:
+        gif="https://media.giphy.com/media/hvYQVRGaGPGDD0bBce/giphy.gif"    
+    return render_template("index.html",output=output, gif=gif)
+
+    
 
    # return jsonify(output)
    # return jsonify(data)
